42db9f1b8ddc48dda07ac7d97d89bbb144ecf937
Siddhartha Khanooja <siddhartha.khanooja@gmail.com>
2019-09-12 09:09:56 +0000

    Updated for probability from emails

--- modules.py
+++ modules.py
@@ -1,78 +1,136 @@
 import re
 import json
+import datefinder
+import editdistance
+import sys
+import speech_recognition as sr
 
-def levenshtein(s1, s2):
-    if len(s1) < len(s2):
-        return levenshtein(s2, s1)
+#def speech_module(filename):
+ #   r = sr.Recognizer()
+  #  harvard = sr.AudioFile('mark.wav')
+   # with harvard as source:
+        # the below command is for removing the noice , in our scenario , its not working fine.
+        # So we are not using it . We can try more cases with it though
+        # r.adjust_for_ambient_noise(source)
+    #    audio = sr.record(source)
+#sr.recognize_google(audio)
 
-    # len(s1) >= len(s2)
-    if len(s2) == 0:
-        return len(s1)
+def text_module_main(filename):
 
-    previous_row = range(len(s2) + 1)
-    for i, c1 in enumerate(s1):
-        current_row = [i + 1]
-        for j, c2 in enumerate(s2):
-            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
-            deletions = current_row[j] + 1       # than s2
-            substitutions = previous_row[j] + (c1 != c2)
-            current_row.append(min(insertions, deletions, substitutions))
-        previous_row = current_row
+    final = []
+    f = open(filename, "r")
+    for line in f:
+        match = re.findall(r'[\w\.-]+@[\w\.-]+\.\w+', line)
+        if len(match) != 0:
+            for x in match:
+                final.append(x)
+
+    f.close()
+
+    final = list(set(final))
+    for e in final:
+        if e.endswith('@gmail.com') or e.endswith('@yahoo.com') or e.endswith('@outlook.com'):
+            customer_email = e
+            break
+
+    print(customer_email)
+    with open('data.json', 'r') as JSON:
+        cList = json.load(JSON)
+
+    #customer_email = "khanu@gmail.com"
+    #noOfDisputes = 0
+    disputes = []
+    for x in cList.values():
+        if x["Email"] == customer_email:
+            reqd_accno = x["AcctNo"]
+        #noOfDisputes += len(x["DisputeID"])
+        for temp in x["DisputeID"]:
+            disputes.append(int(temp))
+
+    print(disputes)
+    noOfDisputes = len(disputes)
+    if(noOfDisputes != 0):
+        maxDisputeID = max(disputes)
+    for x in cList.values():
+        if x["AcctNo"] == reqd_accno:
+            if noOfDisputes == 0:
+                newDisputeID = "0001"
+                x["DisputeID"].append(newDisputeID)
+            else:
+                newDisputeID = (4 - len(str(maxDisputeID)))*'0' + str(maxDisputeID + 1)
+                x["DisputeID"].append(newDisputeID)
     
-    return previous_row[-1]
+    print(customer_email)
+    print(reqd_accno)
+    #print(json.dumps(cList))
+    with open('data.json', 'w') as JSON:
+        json.dump(cList, JSON)
+
+    with open('cases.json', 'r') as JSON:
+        caseList = json.load(JSON)
+
+    print(json.dumps(caseList))
+    probCases = []
+    #descriptions = []
+
+    f = open(filename, "r")
+    f_text = f.read()
+    f.close()
+    normSum = 0.0
+    for x in caseList.values():
+        temp = ''
+        for e in x["Keywords"]:
+            temp += str(e) + '|'
+        temp = temp[:len(temp)-1]
+        re_pattern = r'\b(?:{})'.format(temp)
+        match = re.findall(re_pattern, f_text)
+        prob = round(len(match)/len(x["Keywords"]),2)
+        probCases.append([x["Description"],prob])
+        normSum += prob
+
+    print(probCases)
+    for p in probCases:
+        p[1] = round(p[1]/normSum,2)
+
+    probCases.sort(key = lambda x: x[1], reverse=True)
+    print(probCases)
+    del probCases[2:]
+    print(probCases)
+    
+    new_final = {}
+    new_final[newDisputeID[::-1]] = {"DisputeId": newDisputeID,"ProbCases":probCases}
+    print(new_final)
+    with open('final_bkup.json','r') as JSON:
+        try:
+            final = json.load(JSON)
+        except:
+            final = {}
+    final.update(new_final)
+    with open('final_bkup.json','w') as JSON:
+        json.dump(final, JSON)
+    
+
+if __name__ == "__main__":
+    filename = sys.argv[2]
+    flag = sys.argv[1]
+    if len(sys.argv) >= 2:
+        if filename.endswith('.txt'):
+            #if sys.argv[2] == '--chat':
+                #text_module_chat(filename)
+            if flag == '--email':
+                text_module_main(filename)
+            else:
+                print("Usage: python modules.py --[chat/email] <filename.txt>")
+                exit
+        #else:
+        #    if filename.endswith('.wav'):
+        #        speech_module(filename)
+        #    else:
+        #        print("Usage: python modules.py --audio <filename.wav>")
+        #        break
+    else:
+        print("Usage: python modules.py --[chat/email/audio] <filename.[txt/wav]>")
+        exit
+        
 
 
-with open('cases.json', 'r') as JSON:
-    caseList = json.load(JSON)
-
-print(json.dumps(caseList))
-probCases = []
-
-f = open("sample-dispute-1.txt", "r")
-f_text = f.read()
-f.close()
-for x in caseList.values():
-    temp = ''
-    for e in x["Keywords"]:
-        temp += str(e) + '|'
-    temp = temp[:len(temp)-1]
-    re_pattern = r'\b(?:{})'.format(temp)
-    match = re.findall(re_pattern, f_text)
-    prob = round(len(match)/len(x["Keywords"]),2)
-    probCases.append(prob)
-
-finalProb = []
-for p in probCases:
-    finalProb.append(round(p/sum(probCases),2))
-
-del probCases
-print(finalProb)
-
-final = []
-f = open("sample-dispute-1.txt", "r")
-for line in f:
-    match = re.findall(r'[\w\.-]+@[\w\.-]+\.\w+', line)
-    if len(match) != 0:
-        for x in match:
-            final.append(x)
-
-f.close()
-
-final = list(set(final))
-for e in final:
-    if e.endswith('gmail.com') or e.endswith('yahoo.com') or e.endswith('outlook.com'):
-        customer_email = e
-        break
-
-with open('data.json', 'r') as JSON:
-    cList = json.load(JSON)
-
-customer_email = "sarthak@gmail.com"
-for x in cList.values():
-    if x["Email"] == customer_email:
-        reqd_accno = x["AcctNo"]
-        break
-
-print(customer_email)
-print(reqd_accno)
-print(json.dumps(cList))
